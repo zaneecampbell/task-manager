da5a2a61591d223f34b473c8704d8e320eda3dfc
Zane Campbell <zanegitinit@gmail.com>
2019-03-23 11:51:58 -0700

    setup new collection and instertManny

--- mongodb.js
+++ mongodb.js
@@ -11,8 +11,52 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Zane',
-        age: 27
-    });
+    // db.collection('users').insertOne({
+    //     name: 'Zane',
+    //     age: 27
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user.');
+    //     }
+
+    //     console.log(result.ops);
+    // });
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     },
+    //     {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents')
+    //     }
+
+    //     console.log(result.ops);
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Homework',
+            completed: false
+        },
+        {
+            description: 'Shopping',
+            completed: false
+        },
+        {
+            description: 'Sleep',
+            completed: true
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert tasks!')
+        }
+
+        console.log(result.ops);
+    })
 })
