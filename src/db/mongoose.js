4dac7fd4af9cb8b7a08a18f9f4a3f112d98e6c05
Zane Campbell <zanegitinit@gmail.com>
2019-03-24 22:39:18 -0700

    setup tasks constructor for Tasks collection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -14,13 +14,33 @@ const User = mongoose.model('User', {
     }
 });
 
-const me = new User({
-    name: 'Zane',
-    age: 27
+// const me = new User({
+//     name: 'Zane',
+//     age: 27
+// });
+
+// me.save().then((me) => {
+//     console.log(me);
+// }).catch((error) => {
+//     console.log('Error', error);
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+const task = new Task({
+    description: 'Homeowork',
+    completed: false
 });
 
-me.save().then((me) => {
-    console.log(me);
-}).catch((error) => {
-    console.log('Error', error);
+task.save().then((task) => {
+    console.log(task);
+}).catch((e) => {
+    console.log('Error', e)
 })
