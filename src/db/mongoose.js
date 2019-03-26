c9f0e11d8e67ccd8a2b70270397368bed41e7138
Zane Campbell <zanegitinit@gmail.com>
2019-03-26 12:18:14 -0700

    refactored models and added first rest api route

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,61 +1,9 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { 
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot be password')
-            }
-        }
-    }
-});
-
-// const me = new User({
-//     name: '    Mike     ',
-//     email: 'MYEMAIL@GOOGLE.COM     ',
-//     password: 'pass       '
-// });
-
-// me.save().then((me) => {
-//     console.log(me);
-// }).catch((error) => {
-//     console.log('Error', error);
-// })
 
 const Task = mongoose.model('Task', {
     description: {
@@ -67,14 +15,4 @@ const Task = mongoose.model('Task', {
         type: Boolean,
         default: false
     }
-})
-
-const task = new Task({
-    description: 'Homework      '
-});
-
-task.save().then((task) => {
-    console.log(task);
-}).catch((e) => {
-    console.log('Error', e)
 })
