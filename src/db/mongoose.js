c6cc2da810090b8781381d9dafab3d02d141d9fe
Zane Campbell <zanegitinit@gmail.com>
2019-03-26 21:50:20 -0700

    setup tasks creation endpoint

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,15 +4,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
