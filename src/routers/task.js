0826448b727fa8272b6d2744e34fd8c158eabe3a
Zane Campbell <zanegitinit@gmail.com>
2019-03-30 15:14:50 -0700

    refactored task and user routes

---
+++ src/routers/task.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const Task = require('../models/task')
+const router = new express.Router()
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+        } catch (e) {
+            res.status(400).send(e)
+        }
+});
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(201).send(tasks)
+    } catch (e) {
+        res.status(500).send()
+    }
+});
+
+router.get('/tasks/:id', async (req, res) => {
+    _id = req.params.id;
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id).save()
+        
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+});
+
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ["description", "completed"]
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: "invalid updates" })
+    }
+
+    try {
+        task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return status(404).send()
+        }
+
+        res.status(200).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        return res.status(500).send(e)
+    }
+})
+
+module.exports = router
+
