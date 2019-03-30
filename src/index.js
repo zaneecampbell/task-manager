4bff80d22906966b0795261422f3315c4771c9c9
Zane Campbell <zanegitinit@gmail.com>
2019-03-29 19:10:23 -0700

    added patch endpoints

--- src/index.js
+++ src/index.js
@@ -44,6 +44,28 @@ app.get('/users/:id', async (req, res) => {
     }
 });
 
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+});
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -51,7 +73,7 @@ app.post('/tasks', async (req, res) => {
         await task.save()
         res.status(201).send(task)
         } catch (e) {
-            res.status(400).send()
+            res.status(400).send(e)
         }
 });
 
@@ -68,7 +90,7 @@ app.get('/tasks/:id', async (req, res) => {
     _id = req.params.id;
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findByIdAndUpdate(_id).save()
         
         if (!task) {
             return res.status(404).send()
@@ -79,6 +101,28 @@ app.get('/tasks/:id', async (req, res) => {
         res.status(500).send()
     }
 });
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ["description", "completed"]
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: "invalid updates" })
+    }
+
+    try {
+        task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return status(404).send()
+        }
+
+        res.status(200).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
 
 app.listen(port, () => {
     console.log('Server is up on port ', port);
