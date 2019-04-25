56dcb76410480d8ff677f2e7c2de20acdd29467a
Zane Campbell <zanegitinit@gmail.com>
2019-04-24 21:12:26 -0700

    created first custom tests

--- src/math.js
+++ src/math.js
@@ -3,6 +3,16 @@ const calculateTip = (total, tipPercent = .25) => {
     return total + tip
 }
 
+const fahrenheitToCelsius = (temp) => {
+    return (temp - 32) / 1.8
+}
+
+const celsiusToFahrenheit = (temp) => {
+    return (temp * 1.8) + 32
+}
+
 module.exports = {
-    calculateTip
+    calculateTip,
+    fahrenheitToCelsius,
+    celsiusToFahrenheit
 }
