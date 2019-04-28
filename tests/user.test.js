c1961d2fd7842d2f860f9f8b5f3dca2e75360835
Zane Campbell <zanegitinit@gmail.com>
2019-04-27 19:14:47 -0700

    testing for logining in

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,36 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Zane',
+    email: 'Zane@exmaple.com',
+    password: '56what!!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
         name: 'Zane',
-        email: 'Zaneeeee@example.com',
+        email: 'signup@example.com',
         password: 'MyPass777!'
     }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: 'wrongPassword'
+    }).expect(400)
 })
